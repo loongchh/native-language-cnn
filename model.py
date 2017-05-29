931e780cf6b3f85fafe6956ba064e08787755c8b
Long-Huei Chen <longhuei@stanford.edu>
2017-05-29 20:54:04 +0000

    running on cpu

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -6,35 +7,35 @@ from torch.autograd import Variable
 import numpy as np
 
 
-def _init_weights(self, m):
-        classname = m.__class__.__name__
-        if classname.find('Conv') != -1:
-            m.weight.data.normal_(0.0, 0.02)
-        elif classname.find('BatchNorm') != -1:
-            m.weight.data.normal_(1.0, 0.02)
-            m.bias.data.fill_(0)
+# def weight_init(self, m):
+#         classname = m.__class__.__name__
+#         if classname.find('Conv') != -1:
+#             m.weight.data.normal_(0.0, 0.02)
+#         elif classname.find('Linear') != -1:
+
 
 
 class NativeLanguageCNN(nn.Module):
-    def __init__(self, vocab_size, embed_dim, out_channel, n_language):
-        super(CNN, self).__init__()
-        self.embed = nn.Embedding(vocab_size, embed_dim)
+    def __init__(self, vocab_size, embed_dim, dropout, out_channel, n_language):
+        super(NativeLanguageCNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size + 1, embed_dim, padding_idx=vocab_size)
+        self.dropout = nn.Dropout(dropout, inplace=True)
 
-        self.cnn3 = nn.Conv1d(1, out_channel, 3)
-        self.cnn4 = nn.Conv1d(1, out_channel, 4)
-        self.cnn5 = nn.Conv1d(1, out_channel, 5)
+        self.cnn3 = nn.Conv2d(1, out_channel, (3, embed_dim))
+        self.cnn4 = nn.Conv2d(1, out_channel, (4, embed_dim))
+        self.cnn5 = nn.Conv2d(1, out_channel, (5, embed_dim))
 
         self.linear = nn.Linear(out_channel * 3, n_language)
 
-        self.weight_init()
+        # self.apply(weight_init)
 
     def forward(self, x):
-        embedding = self.embed(x)
+        embedding = self.dropout(self.embed(x)).unsqueeze(1)
 
-        h3 = self.cnn3(embedding).max(dim=2)
-        h4 = self.cnn4(embedding).max(dim=2)
-        h5 = self.cnn5(embedding).max(dim=2)
+        h3 = self.cnn3(embedding).max(dim=2)[0].squeeze(-1)
+        h4 = self.cnn4(embedding).max(dim=2)[0].squeeze(-1)
+        h5 = self.cnn5(embedding).max(dim=2)[0].squeeze(-1)
 
-        h_cnn = torch.cat((h3, h4, h5), dim=1)
+        h_cnn = torch.cat((h3, h4, h5), dim=1).squeeze(-1)
         out = self.linear(h_cnn)
         return out
--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 import argparse
 from os import listdir
 from os.path import join
+import logging
 import pickle
-from tqdm import tqdm
+from tqdm import trange
 
 import torch
 import torch.nn as nn
@@ -11,13 +12,16 @@ from torch.autograd import Variable
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 from model import NativeLanguageCNN
 
 
-def read_mat(file_dir, max_length):
+def read_mat(file_dir, max_length, vocab_size):
     file_list = listdir(file_dir)
-    mat = -np.ones((len(file_list), max_length))
+
+    # vocab_size indices stands for padding
+    mat = np.ones((len(file_list), max_length), dtype=np.int64) * vocab_size
 
     for (i, fl) in enumerate(file_list):
         tokens = open(join(file_dir, fl)).read().split()
@@ -27,61 +31,114 @@ def read_mat(file_dir, max_length):
     return mat
 
 
-def train(args):
-    train_mat = read_mat(join(args.feature_dir, 'train'), args.max_length)
+def train(args, logger=None):
+    with open(join(args.feature_dir, 'dict.pkl'), 'rb') as fpkl:
+        (feature_dict, feature_rev_dict) = pickle.load(fpkl)
+    n_features = len(feature_dict)
+    logger.debug("number of features = {}".format(n_features))
+
+    logger.info("Reading train dataset...")
+    train_mat = read_mat(join(args.feature_dir, 'train'),
+                         args.max_length, n_features)
+
+    logger.info("Reading train labels...")
     train_lang = pd.read_csv(args.label)['L1'].values.tolist()
     lang_list = sorted(list(set(train_lang)))
+    logger.debug("list of L1: {}".format(lang_list))
     lang_dict = {i: l for (i, l) in enumerate(lang_list)}
     lang_rev_dict = {l: i for (i, l) in lang_dict.items()}
     train_label = [lang_rev_dict[la] for la in train_lang]
 
-    with open(join(args.feature_dir, 'dict_rev_dict.pkl'), 'rb') as fpkl:
-        (feature_dict, feature_rev_dict) = pickle.load(fpkl)
-    n_features = len(feature_dict)
+    (train_mat, val_mat, train_label, val_label) = \
+        train_test_split(train_mat, train_label, test_size=args.val_split)
+    logger.debug("created train set of size {}, val set of size {}".format(
+        train_mat.shape[0], val_mat.shape[0]))
 
-    nl_cnn_model = NativeLanguageCNN(n_features, args.embed_dim, args.channel, len(lang_list))
+    logger.info("Constructing CNN model...")
+    nl_cnn = NativeLanguageCNN(n_features, args.embed_dim, args.dropout,
+                               args.channel, len(lang_list))
     if args.gpu:
-        model.cuda()
+        logger.info("Enabling GPU computation...")
+        nl_cnn.cuda()
 
-    optimizer = optim.Adam(model.parameters(), lr=args.lr,
+    logger.info("Creating optimizer...")
+    optimizer = optim.Adam(nl_cnn.parameters(), lr=args.lr,
                            weight_decay=args.regularization)
+    logger.debug("list of parameters: {}".format(list(zip(*nl_cnn.named_parameters()))[0]))
     criterion = nn.CrossEntropyLoss()
 
-    #TODO: split out validation set
     train_dataset = TensorDataset(torch.from_numpy(train_mat), torch.LongTensor(train_label))
-    train_data_loader = DataLoader(train_dataset)
+    train_data_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_mat_var = Variable(torch.from_numpy(val_mat))
 
-    for ep in tqdm(range(args.num_epochs)):
-        for t, (x, y) in enumerate(train_data_loader):
-            if args.gpu:
-                x = x.cuda()
-                y = y.cuda()
+    train_acc = []
+    train_loss = []
+    val_acc = []
+    val_loss = []
+    num_steps = args.num_epochs * int(np.ceil(train_mat.shape[0] / args.batch_size))
 
-            x_var = Variable(x)
-            y_var = Variable(y)
-            score = nl_cnn_model(x_var)
-            loss = criterion(score, y_var)
+    with trange(num_steps) as progbar:
+        for ep in range(args.num_epochs):
+            print("Epoch #{:d} of {:d}".format(ep + 1, args.num_epochs))
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            for (t, (x, y)) in enumerate(train_data_loader):
+                if args.gpu:
+                    x = x.cuda()
+                    y = y.cuda()
 
-    #TODO: print out losses every epoch
-    #TODO: evaluate on val set
+                x_var = Variable(x)
+                y_var = Variable(y)
+                nl_cnn.train()
+                score = nl_cnn(x_var)
+                pred = np.argmax(score.data.cpu().numpy(), axis=1)
+                train_acc.append(np.mean(pred == train_label))
+
+                loss = criterion(score, y_var)
+                train_loss.append(loss.data.cpu().numpy())
+                # logger.debug("train loss = {:4f}, acc = {:4f}".format(train_loss[-1], train_acc[-1]))
+
+                optimizer.zero_grad()
+                loss.backward()
+                if args.clip_norm:
+                    norm = nn.utils.clip_grad_norm(nl_cnn.parameters, args.clip_norm)
+                    progbar.set_postfix(loss=train_loss[-1], accuracy=train_acc[-1], norm=norm)
+                    # logger.debug("grad norm = {:4f}".format(norm))
+                else:
+                    progbar.set_postfix(loss=train_loss[-1], accuracy=train_acc[-1])
+
+                progbar.update(1)
+                optimizer.step()
+
+            if args.debug or (ep > 0 and (ep % args.evaluate_every) == 0):
+                logger.info("Evaluating...")
+                val_score = nl_cnn(val_mat_var)
+                val_pred = np.argmax(val_score.data.cpu().numpy(), axis=1)
+                val_acc.append(np.mean(val_pred == val_label))
+                logger.info("Epoch #{:d} val accuracy = {:4f}".format(val_acc))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CNN')
-    parser.add_argument('--lr', type=float, default=1e-3,
+    parser.add_argument('--lr', type=float, default=1e-4,
                         help='learning rate')
+    parser.add_argument('--seed', type=int, default=224,
+                        help='seed for random initialization')
     parser.add_argument('--regularization', type=float, default=1e-4,
                         help='regularization coefficient')
+    parser.add_argument('--clip-norm', type=float, default=None,
+                        help='clip by total norm')
+    parser.add_argument('--dropout', type=float, default=0.25,
+                        help='dropout strength')
     parser.add_argument('--num-epochs', type=int, default=100,
                         help='number of training epochs')
     parser.add_argument('--batch-size', type=int, default=32,
                         help='size of mini-batch')
-    parser.add_argument('--max-length', type=int, default=500,
+    parser.add_argument('--val-split', type=float, default=0.0909,
+                        help='fraction of train set to use as val set')
+    parser.add_argument('--max-length', type=int, default=600,
                         help='maximum feature length for each document')
+    parser.add_argument('--evaluate-every', type=int, default=10,
+                        help='frequency of which evaluation is done with the val set')
     parser.add_argument('--embed-dim', type=int, default=500,
                         help='dimension of the feature embeddings')
     parser.add_argument('--channel', type=int, default=500,
@@ -91,7 +148,17 @@ if __name__ == '__main__':
                               pickle file of (dict, rev_dict) mapping indices to feature labels')
     parser.add_argument('--label', type=str, default='data/labels/train/labels.train.csv',
                         help='CSV of the train set labels')
+    parser.add_argument('--debug', action='store_true',
+                        help='enable debugging messages')
     parser.add_argument('--gpu', action='store_true',
                         help='using GPU-enabled CUDA Variables')
+
     args = parser.parse_args()
-    train(args)
+
+    logging.basicConfig(level=(logging.DEBUG if args.debug else logging.INFO))
+    logger = logging.getLogger("train")
+
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    train(args, logger)
