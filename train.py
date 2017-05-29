8432aec8b156049cbcf4ecbcd5e5c32c8ea7d099
Long-Huei Chen <longhuei@stanford.edu>
2017-05-28 21:09:29 -0700

    Remove unused dev set in train.py

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ from model import NativeLanguageCNN
 
 
 def read_mat(file_dir, max_length):
-    file_list = os.listdir(file_dir)
+    file_list = listdir(file_dir)
     mat = -np.ones((len(file_list), max_length))
 
     for (i, fl) in enumerate(file_list):
@@ -29,16 +29,11 @@ def read_mat(file_dir, max_length):
 
 def train(args):
     train_mat = read_mat(join(args.feature_dir, 'train'), args.max_length)
-    dev_mat = read_mat(join(args.feature_dir, 'dev'), args.max_length)
-
-    lang_list = sorted(list(set(dev_label)))
-    lang_dict = {i: l for (i, l) in enumerate(language_list)}
+    train_lang = pd.read_csv(args.label)['L1'].values.tolist()
+    lang_list = sorted(list(set(train_lang)))
+    lang_dict = {i: l for (i, l) in enumerate(lang_list)}
     lang_rev_dict = {l: i for (i, l) in lang_dict.items()}
-
-    train_lang = pd.read_csv(args.train_label)['L1'].values.tolist()
-    dev_lang = pd.read_csv(args.dev_label)['L1'].values.tolist()
     train_label = [lang_rev_dict[la] for la in train_lang]
-    dev_label = [lang_rev_dict[la] for la in dev_lang]
 
     with open(join(args.feature_dir, 'dict_rev_dict.pkl'), 'rb') as fpkl:
         (feature_dict, feature_rev_dict) = pickle.load(fpkl)
@@ -53,7 +48,7 @@ def train(args):
     criterion = nn.CrossEntropyLoss()
 
     #TODO: split out validation set
-    train_dataset = TensorDataset(torch.from_numpy(train_mat), torch.IntTensor(train_label))
+    train_dataset = TensorDataset(torch.from_numpy(train_mat), torch.LongTensor(train_label))
     train_data_loader = DataLoader(train_dataset)
 
     for ep in tqdm(range(args.num_epochs)):
@@ -63,7 +58,7 @@ def train(args):
                 y = y.cuda()
 
             x_var = Variable(x)
-            y_var = Variable(y.long())
+            y_var = Variable(y)
             score = nl_cnn_model(x_var)
             loss = criterion(score, y_var)
 
@@ -94,10 +89,8 @@ if __name__ == '__main__':
     parser.add_argument('--feature-dir', type=str, default='data/features/speech_transcriptions/ngrams/2/',
                         help='directory containing features, including train/dev directories and \
                               pickle file of (dict, rev_dict) mapping indices to feature labels')
-    parser.add_argument('--train-label', type=str, default='data/labels/train/labels.train.csv',
+    parser.add_argument('--label', type=str, default='data/labels/train/labels.train.csv',
                         help='CSV of the train set labels')
-    parser.add_argument('--dev-label', type=str, default='data/labels/dev/labels.dev.csv',
-                        help='CSV of the dev set labels')
     parser.add_argument('--gpu', action='store_true',
                         help='using GPU-enabled CUDA Variables')
     args = parser.parse_args()
