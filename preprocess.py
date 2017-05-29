64f5aab3f16a409aa8e81f247187a101790859b8
Long-Huei Chen <longhuei@stanford.edu>
2017-05-29 03:12:06 +0000

    create histograms, generate monograms, new dictionary pickle files with (dict, rev_dict) tuples

--- preprocess.py
+++ preprocess.py
@@ -19,31 +19,32 @@ def preprocess(args):
                                  str(args.ngram), args.data)
         ngram_list = product(ascii_lowercase + punctuation + digits,
                              repeat=args.ngram)
-        ngram_dict = {ng: i for (i, ng) in enumerate(ngram_list)}
+        ngram_dict = {i: ng for (i, ng) in enumerate(ngram_list)}
+        ngram_rev_dict = {ng: i for (i, ng) in ngram_dict.items()}
         pickle_path = os.path.join("data/features/speech_transcriptions/ngrams",
                                    str(args.ngram), 'ngram_dict.pkl')
         with open(pickle_path, 'wb') as fpkl:
-            pickle.dump(ngram_dict, fpkl)
+            pickle.dump((ngram_dict, ngram_rev_dict), fpkl)
 
     if args.arpabet:
-        cmu_dict = nltk.corpus.cmudict.dict()
+        cmu_rev_dict = nltk.corpus.cmudict.dict()
         arpabet_dir = os.path.join("data/features/speech_transcriptions/arpabets/",
                                    args.data)
         arpabet_list = ['AA', 'AE', 'AH', 'AO', 'AW', 'AY', 'B', 'CH', 'D',
                         'DH', 'EH', 'ER', 'EY', 'F', 'G', 'HH', 'IH', 'IY',
                         'JH', 'K', 'L', 'M', 'N', 'NG', 'OW', 'OY', 'P', 'R',
                         'S', 'SH', 'T', 'TH', 'UH', 'UW', 'V', 'W', 'Y', 'Z', 'ZH']
-
-        arpabet_dict = {a: i for (i, a) in enumerate(arpabet_list)}
+        arpabet_dict = {i: a for (i, a) in enumerate(arpabet_list)}
+        arpabet_rev_dict = {a: i for (i, a) in arpabet_dict.items()}
         pickle_path = os.path.join("data/features/speech_transcriptions/arpabets/",
                                    'arpabet_dict.pkl')
         with open(pickle_path, 'wb') as fpkl:
-            pickle.dump(arpabet_dict, fpkl)
+            pickle.dump((arpabet_dict, arpabet_rev_dict), fpkl)
 
         def get_arpabet_idx(arpabet):
             if arpabet[-1].isdigit():
                 arpabet = arpabet[:-1]
-            return arpabet_dict[arpabet]
+            return arpabet_rev_dict[arpabet]
 
     for fn in tqdm(file_list):
         fullpath = os.path.join(transcript_dir, fn)
@@ -57,18 +58,18 @@ def preprocess(args):
                 if args.ngram:
                     ngram = ngrams(''.join(line.lower().split()), n=args.ngram)
                     # for ng in ngram:
-                    #     if ng not in ngram_dict:
+                    #     if ng not in ngram_rev_dict:
                     #         print(line)
                     #         continue
-                    seq = [ngram_dict[ng] for ng in ngram if ng in ngram_dict]
-                    f_ngram.writelines(' '.join(str(i) for i in seq))
+                    seq = [ngram_rev_dict[ng] for ng in ngram if ng in ngram_rev_dict]
+                    f_ngram.writelines(' '.join(str(i) for i in seq) + '\n')
 
                 if args.arpabet:
-                    arpabets = [cmu_dict[word][0] for word in line.split()
-                                if word in cmu_dict]
+                    arpabets = [cmu_rev_dict[word][0] for word in line.split()
+                                if word in cmu_rev_dict]
 
                     seq = [get_arpabet_idx(ab) for wd in arpabets for ab in wd]
-                    f_arpabet.writelines(' '.join(str(i) for i in seq))
+                    f_arpabet.writelines(' '.join(str(i) for i in seq) + '\n')
 
             if args.ngram:
                 f_ngram.close()
