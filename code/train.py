a380191fc1cfdeedf9e582c479f8e9f40879ad43
Long-Huei Chen <longhuei@stanford.edu>
2017-06-05 10:28:46 -0700

    Add hyperparameter search script

--- code/train.py
+++ code/train.py
@@ -16,6 +16,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 
+from model import NativeLanguageCNN
+
 
 def read_data(file_dir, label_file, val_split, vocab_size, max_len, sen_len=None, logger=None):
     """ Read train matrices and labels from specified directory.
@@ -240,8 +242,6 @@ def train(args, save_dir=None, logger=None, progbar=True):
 
 
 if __name__ == '__main__':
-    from model import NativeLanguageCNN
-
     parser = argparse.ArgumentParser(description='NLCNN')
     parser.add_argument('--lr', type=float, default=1e-4,
                         help='learning rate')
